1d1e61cb0ad4f878ef9762e8f04365f98f349237
prabin3140 <prabin3140@gmail.com>
2022-11-16 11:26:29 +0530

    Dev branch push

--- src/main/java/com/ge/devops/Welcome.java
+++ src/main/java/com/ge/devops/Welcome.java
@@ -8,6 +8,6 @@ public class Welcome
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome World!" );
+        System.out.println( "Welcome World Dev Branch!" );
     }
 }
